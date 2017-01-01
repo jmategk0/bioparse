e5034db431101c0bf9ec047cee2a2b468ff7a9f4
jmategk0 <jmategkox2@gmail.com>
2016-12-31 20:40:03 -0800

    Add genbank_to_dictionary feature and test data.

--- bioparse.py
+++ bioparse.py
@@ -1,12 +1,120 @@
-from Bio.Seq import Seq
+from Bio import SeqIO
 
 
-class Sequence(object):
+class Genbank(object):
 
     def __init__(self):
-        self.seq_str = ""
+        self.filename = ""
 
-    def get_seq(self, string_value):
-        my_seq = Seq(string_value)
-        return my_seq
+    def object_to_dictionary(self, value):
 
+        return value.__dict__  # value.__dict__  # vars(value)
+
+    def clean_featurelocation(self, featurelocations):
+
+        if isinstance(featurelocations, list):
+            locations = []
+            for featurelocation in featurelocations:
+                featurelocation_dict = self.object_to_dictionary(value=featurelocation)
+                featurelocation_dict["_start"] = int(featurelocation_dict["_start"])
+                featurelocation_dict["_end"] = int(featurelocation_dict["_end"])
+                locations.append(featurelocation_dict)
+            return_value = locations
+        else:
+            featurelocation_dict = self.object_to_dictionary(value=featurelocations)
+            featurelocation_dict["_start"] = int(featurelocation_dict["_start"])
+            featurelocation_dict["_end"] = int(featurelocation_dict["_end"])
+            return_value = featurelocation_dict
+        return return_value
+
+    def clean_seqfeatures(self, seqfeatures):
+        feature_list = []
+
+        for feature in seqfeatures:
+            feature_dict = self.object_to_dictionary(value=feature)
+
+            if feature_dict['id'] == '<unknown id>':
+                # Set "unknown id" to None to be more pythonic and better support json serialisation.
+                feature_dict['id'] = None
+                feature_dict['location'] = self.clean_featurelocation(feature_dict['location'])
+
+            feature_list.append(feature_dict)
+        return feature_list
+
+    def clean_annotation_references(self, annotation_references):
+
+        references = []
+        for reference in annotation_references:
+            reference_dict = self.object_to_dictionary(value=reference)
+            if reference_dict["location"]:
+                reference_dict["location"] = self.clean_featurelocation(reference_dict["location"])
+            references.append(reference_dict)
+        return references
+
+    def clean_seq(self, biopython_seq):
+        biopython_seq_dict = self.object_to_dictionary(value=biopython_seq)
+        biopython_seq_dict["alphabet"] = biopython_seq_dict["alphabet"].letters
+        return biopython_seq_dict
+
+    def genbank_to_dictionary(self, genbank_file):
+
+        clean_genbank_records = []
+
+        for genbank_record_as_seq_record_obj in SeqIO.parse(genbank_file, "genbank"):
+
+            raw_seq_record_dict = self.object_to_dictionary(value=genbank_record_as_seq_record_obj)
+
+            raw_seq_record_dict["_seq"] = self.clean_seq(raw_seq_record_dict["_seq"])
+
+            if raw_seq_record_dict["annotations"]["references"]:
+                raw_seq_record_dict["annotations"]["references"] = self.clean_annotation_references(
+                    raw_seq_record_dict["annotations"]["references"])
+
+            if raw_seq_record_dict["features"]:
+                raw_seq_record_dict["features"] = self.clean_seqfeatures(raw_seq_record_dict["features"])
+
+            clean_genbank_records.append(raw_seq_record_dict)
+
+        return clean_genbank_records
+
+    def genbank_to_dictionary_lite(self, genbank_file):
+
+        clean_genbank_records = []
+
+        for genbank_record_as_seq_record_obj in SeqIO.parse(genbank_file, "genbank"):
+
+            raw_seq_record_dict = genbank_record_as_seq_record_obj.__dict__
+
+            raw_seq_record_dict["_seq"] = raw_seq_record_dict["_seq"].__dict__
+            raw_seq_record_dict["_seq"]["alphabet"] = raw_seq_record_dict["_seq"]["alphabet"].letters
+
+            if raw_seq_record_dict["annotations"]["references"]:
+                references = []
+                for reference in raw_seq_record_dict["annotations"]["references"]:
+                    reference_dict = reference.__dict__
+                    if reference_dict["location"]:
+                        feature_locations = []
+
+                        for feature_location in reference_dict["location"]:
+                            feature_location_dict = feature_location.__dict__
+                            feature_location_dict["_start"] = int(feature_location_dict["_start"])
+                            feature_location_dict["_end"] = int(feature_location_dict["_end"])
+                            feature_locations.append(feature_location_dict)
+                            reference_dict["location"] = feature_locations
+                    references.append(reference.__dict__)
+                raw_seq_record_dict["annotations"]["references"] = references
+
+            if raw_seq_record_dict["features"]:
+                features = []
+                for feature in raw_seq_record_dict["features"]:
+                    feature_dict = feature.__dict__
+                    if feature_dict["id"] == '<unknown id>':
+                        feature_dict["id"] = None
+                    feature_dict["location"] = feature_dict["location"].__dict__
+                    feature_dict["location"]["_start"] = int(feature_dict["location"]["_start"])
+                    feature_dict["location"]["_end"] = int(feature_dict["location"]["_end"])
+                    features.append(feature_dict)
+                raw_seq_record_dict["features"] = features
+            clean_genbank_records.append(raw_seq_record_dict)
+
+        return clean_genbank_records
--- bioparseTests.py
+++ bioparseTests.py
@@ -1,7 +1,29 @@
 import bioparse
+import time
+# import json
 
-seq = bioparse.Sequence()
+# from pprint import pprint
+parser = bioparse.Genbank()
 
-var = seq.get_seq("CATGTAGACTAG")
 
-print(var)
+start_time = time.time()
+# d = parser.genbank_to_dictionary(genbank_file="test_files/biopython/ls_orchid.gbk")
+d = parser.genbank_to_dictionary(genbank_file="test_files/biopython/ls_orchid.gbk")
+end_time = time.time()
+# e = json.dumps(d)
+
+# pprint(d)
+print(type(d))
+print("--- %s seconds ---" % (end_time - start_time))
+# parser.genbank_to_dictionary
+# --- 0.03371381759643555 seconds ---
+# --- 0.03315591812133789 seconds ---
+# --- 0.03240847587585449 seconds ---
+
+
+# parser.genbank_to_dictionary_lite
+# --- 0.03237748146057129 seconds ---
+# --- 0.03287935256958008 seconds ---
+# --- 0.03213834762573242 seconds ---
+
+# diff is ~0.001 seconds. Will this matter at scale? What is more readable?
